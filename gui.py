3b85d7be10f4c93ed96906909de2143d69cd0ce7
Ishani  Madhuwanthi <ishmadhuwanthi@gmail.com>
2020-10-05 10:30:56 +0530

    tkinter entry

--- gui.py
+++ gui.py
@@ -1,46 +1,51 @@
-# import tkinter as tk
-# from tkinter import filedialog, Text
-# import os
-#
-# root = tk.Tk()
-#
-# canvas = tk.Canvas(root, height=500, width=700, bg='blue')
-# canvas.pack()
-#
-# frame = tk.Frame(root, bg="white")
-# frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-#
-# root.mainloop()
-
 from tkinter import *
 
+class Gui:
+    def __init__(self):
+        window = Tk()
+        # add widgets here
 
-window = Tk()
-# add widgets here
+        window.title("Eggs Encubator-Scada System")
+        window.geometry("600x300")
+        # window.configure(bg='#856ff8')
 
-window.title('Eggs Encubator-Scada System')
-window.geometry("500x300")
+        lbl0=Label(window, text="Incubator System", fg='red', font=("Helvetica", 16))
+        lbl0.place(x=200, y=20)
 
-lbl0=Label(window, text="Incubator System", fg='red', font=("Helvetica", 16))
-lbl0.place(x=90, y=20)
+        #indictor column
+        col1=Label(window, text="Indicator", fg='Green', font=("Helvetica", 16))
+        col1.place(x=300, y=60)
 
-col1=Label(window, text="Indicator", fg='Green', font=("Helvetica", 16))
-col1.place(x=200, y=60)
+        #controller column
+        col2=Label(window, text="Controller", fg='green', font=("Helvetica", 16))
+        col2.place(x=450, y=60)
 
-col2=Label(window, text="Controller", fg='green', font=("Helvetica", 16))
-col2.place(x=350, y=60)
+        #temperature
+        lbl1=Label(window, text="Temperature", fg='Black', font=("Helvetica", 16))
+        lbl1.place(x=10, y=100)
+        entryTemp = Entry(window,width=15)
+        entryTemp.place(x=180, y=110)
 
-lbl1=Label(window, text="Temperature", fg='Black', font=("Helvetica", 16))
-lbl1.place(x=10, y=100)
+        #pressure
+        lbl2=Label(window, text="Pressure", fg='Black', font=("Helvetica", 16))
+        lbl2.place(x=10, y=150)
+        entryPre = Entry(window, width=15)
+        entryPre.place(x=180, y=160)
 
-lbl2=Label(window, text="Pressure", fg='Black', font=("Helvetica", 16))
-lbl2.place(x=10, y=150)
+        #light intensity
+        lbl3=Label(window, text="Light Intensity", fg='Black', font=("Helvetica", 16))
+        lbl3.place(x=10, y=200)
+        entryLight = Entry(window, width=15)
+        entryLight.place(x=180, y=210)
 
-lbl3=Label(window, text="Light Intensity", fg='Black', font=("Helvetica", 16))
-lbl3.place(x=10, y=200)
+        #change button
+        endlbl=Label(window, text="Change",fg='Black', font=("Helvetica", 16))
+        endlbl.place(x=100, y=250)
 
-endlbl=Label(window, text="Change", fg='Black', font=("Helvetica", 16))
-endlbl.place(x=100, y=250)
+        btn = Button(window, text = 'Click me !', bg='blue', fg='white', command = window.destroy, width=10)
+        btn.place(x=250, y=250)
+
+        window.mainloop()
 
 
-window.mainloop()
+Gui()
